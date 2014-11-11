9927f5bf5ff61321fadeecd258f9c74fca4c2e3c
Blake Mesdag <blakemesdag@gmail.com>
2014-11-11 18:46:19 -0500

    Add concurrency handling, and wait time

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 import "net/http"
 import "bufio"
 import "os"
+import "sync"
+import "time"
 
 func HitUrl(url string) (byte, error) {
 	resp, err := http.Get(url)
@@ -15,7 +17,7 @@ func HitUrl(url string) (byte, error) {
 	defer resp.Body.Close()
 	r := bufio.NewReader(resp.Body)
 	if err != nil {
-		fmt.Sprintf("Error initialize scanner for %s: %s", url, err)
+		fmt.Sprintln("Error initialize scanner for %s: %s", url, err)
 	}
 
 	return r.ReadByte()
@@ -37,23 +39,53 @@ func GetUrlBatch(r *bufio.Scanner, size int) ([]string, int) {
 	return batch, length
 }
 
-func main() {
-	urlFilePath := flag.String("file", "urls.csv", "path to file containing urls to warm")
-	flag.Parse()
-
-	fmt.Println("Loading urls from: ", *urlFilePath)
-
-	urlFile, err := os.Open(*urlFilePath)
+func SetupScanner(filepath string) (*bufio.Scanner, *os.File) {
+	urlFile, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println("Error opening file: ", err)
 	}
 
 	scanner := bufio.NewScanner(urlFile)
 
-	currBatch, length := GetUrlBatch(scanner, 2)
-	for i := 0; i < length; i++ {
-		fmt.Println("Warming cache with url: ", currBatch[i])
-		go HitUrl(currBatch[i])
+	return scanner, urlFile
+}
+
+func HitUrlsInBatches(s *bufio.Scanner, batchSize int) int {
+	var wg sync.WaitGroup
+	totalHit := 0
+
+	currBatch, length := GetUrlBatch(s, batchSize)
+	for length > 0 {
+		for i := 0; i < length; i++ {
+			wg.Add(1)
+
+			fmt.Println("Warming cache with url: ", currBatch[i])
+			go func(wg *sync.WaitGroup, url string) {
+				HitUrl(url)
+				totalHit++
+				wg.Done()
+			}(&wg, currBatch[i])
+		}
+
+		wg.Wait()
+		time.Sleep(500 * time.Millisecond)
+
+		currBatch, length = GetUrlBatch(s, batchSize)
 	}
-	defer urlFile.Close()
+
+	return totalHit
+}
+
+func main() {
+	urlFilePath := flag.String("file", "urls.csv", "path to file containing urls to warm")
+	flag.Parse()
+
+	fmt.Println("Loading urls from: ", *urlFilePath)
+	scanner, urlFile := SetupScanner(*urlFilePath)
+
+	totalHit := HitUrlsInBatches(scanner, 2)
+
+	fmt.Sprintln("Warmed cache with %i urls", totalHit)
+
+	urlFile.Close()
 }
