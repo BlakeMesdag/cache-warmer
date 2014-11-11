8711589955deea4db7676568bc8af656906cc2ff
Blake Mesdag <blakemesdag@gmail.com>
2014-11-11 16:47:54 -0500

    Rename functions

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "net/http"
 import "bufio"
 import "os"
 
-func getUrl(url string) (byte, error) {
+func HitUrl(url string) (byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Sprintf("Error getting %s: %s", url, err)
@@ -21,7 +21,7 @@ func getUrl(url string) (byte, error) {
 	return r.ReadByte()
 }
 
-func getUrlBatch(r *bufio.Scanner, size int) ([]string, int) {
+func GetUrlBatch(r *bufio.Scanner, size int) ([]string, int) {
 	var batch = make([]string, 0)
 	length := 0
 
@@ -50,10 +50,10 @@ func main() {
 
 	scanner := bufio.NewScanner(urlFile)
 
-	currBatch, length := getUrlBatch(scanner, 2)
+	currBatch, length := GetUrlBatch(scanner, 2)
 	for i := 0; i < length; i++ {
 		fmt.Println("Warming cache with url: ", currBatch[i])
-		go getUrl(currBatch[i])
+		go HitUrl(currBatch[i])
 	}
 	defer urlFile.Close()
 }
